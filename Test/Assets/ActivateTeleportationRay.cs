ba152d7fc05d0af9216fa062a4928f35c68e3be8
Alexanity <80948589+Alexanity@users.noreply.github.com>
2023-08-18 23:19:00 +0300

    Interactables
    Cubes and a gun

--- Test/Assets/ActivateTeleportationRay.cs
+++ Test/Assets/ActivateTeleportationRay.cs
@@ -9,9 +9,11 @@ public class ActivateTeleportationRay : MonoBehaviour
 
     public GameObject rightTeleportation;
 
+    public InputActionProperty rightCancel;
+
     public InputActionProperty rightActivate;
     void Update()
     {
-        rightTeleportation.SetActive(rightActivate.action.ReadValue<float>() > 0.1f);
+        rightTeleportation.SetActive(rightCancel.action.ReadValue<float>()==0 && rightActivate.action.ReadValue<float>() > 0.1f);
     }
 }
