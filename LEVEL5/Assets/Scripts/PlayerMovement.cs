577e32afe959230b6580c2ff6f74799ae96616fd
Alexanity <80948589+Alexanity@users.noreply.github.com>
2023-11-09 22:06:28 +0200

    Adding Velocity and GroundCheck

--- LEVEL5/Assets/Scripts/PlayerMovement.cs
+++ LEVEL5/Assets/Scripts/PlayerMovement.cs
@@ -7,12 +7,32 @@ public class PlayerMovement : MonoBehaviour
     // reference to the character Controller
     public CharacterController controller;
 
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+
     // movement speed
     public float speed = 12f;
+
+    public float gravity = -9.81f;
+
+    // Gradually picking up speed as the player falls down
+    Vector3 velocity;
 
     // Update is called once per frame
     void Update()
     {
+        // Creates a little sphere checking if it's colliding with anything, if yes then it's true, if not then it's false
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        // If we're on the ground -> reset the velocity
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+
         // Input
         float x = Input.GetAxis("Horizontal"); // W&S
         float z = Input.GetAxis("Vertical");   // A&D
@@ -25,5 +45,10 @@ public class PlayerMovement : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z; // movement based on where we're looking at
 
         controller.Move(move * speed * Time.deltaTime);
+
+        // building up the velocity
+        velocity.y += gravity * Time.deltaTime;
+        // moving the controller downwards
+        controller.Move(velocity * Time.deltaTime);
     }
 }
